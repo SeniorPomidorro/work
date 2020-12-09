1be98d06d14c2c3f2ba01358c98a83203493d37b
SeniorPomidorro <68108903+SeniorPomidorro@users.noreply.github.com>
2020-12-09 13:13:50 +0300

    Update firs steps.py

--- firs steps.py
+++ firs steps.py
@@ -1,9 +1,9 @@
 import requests as r
 
-BOT_TOKEN = "800123310:AAEqhwlZNivTsfGCf6s0tSjC-6X23Z5SwaU"
+BOT_TOKEN = ""
 
 json = {"chat_id":-471798724,
 "text":"мы тут"}
 
-req = r.post('https://api.telegram.org/bot800123310:AAEqhwlZNivTsfGCf6s0tSjC-6X23Z5SwaU/sendMessage', json=json)
+req = r.post('https://api.telegram.org//sendMessage', json=json)
 print(req.text)
